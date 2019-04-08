5e33ec8dc4550fc75938cb8b2ea8cf2ac266f3ec
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-04-08 19:57:19 -0400

    Remodeled function to be generalize, starting to think it may be an unneccesary function

--- qmspy/get_errs.py
+++ qmspy/get_errs.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-def get_errs(high, low):
+def get_errs(datafile, header):
     """
-    ????.
+    This fucntion gets the stadarad deviation of the QMS data given to it.
 
     Parameters
     ----------
@@ -16,18 +16,9 @@ def get_errs(high, low):
     -------
 
     """
-    #Data Frame Columns
-    amu       =  'mass amu'
-    sem       =  'SEM Amps'
-    ev        =  'electron-energy V'
-    cyc       =  'Cycle'
 
-    hi = pd.read_csv(high, header=22)
-    lo = pd.read_csv(low,  header=22)
+    data = pd.read_csv(datafile, header=header)
 
-    stdhi = hi.groupby([ev, amu]).std()
-    stdlo = lo.groupby([ev, amu]).std()
+    std = hi.groupby([ev, amu]).std()
 
-    avgstd = (stdhi[sem] + stdlo[sem]) / 2
-
-    return stdhi, stdlo
+    return std
