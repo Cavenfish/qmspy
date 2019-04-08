39b37edec42230f6807fa851e3cbbf14b5231d34
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-04-08 19:52:13 -0400

    This will be a function for integrating each Data Peak from QMS data

---
+++ qmspy/integrate.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+def integrate(data, peaks):
+    """
+    ??????.
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+
+
+    Example
+    -------
+
+    """
