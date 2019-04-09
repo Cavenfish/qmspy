4da1558a64b9f9a47114eb06ced5cfa1af7f18b2
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-04-08 20:09:55 -0400

    Doc-string edit

--- qmspy/__init__.py
+++ qmspy/__init__.py
@@ -6,13 +6,21 @@ Contains the following functions:
                 init_data
                 get_errs
                 add_style
-                line
-                bar
-                scatter
-                dddbar
-                dddline
+
+Contains the following submodules:
+                graph_data
 
 
 Author: Brian C. Ferrari
 """
+import numpy as np
+import pandas as pd
 
+#Data Frame Columns
+amu       =  'mass amu'
+sem       =  'SEM Amps'
+ev        =  'electron-energy V'
+cyc       =  'Cycle'
+col       =  'Color'
+mar       =  'Marker'
+std       =  'Standard Deviation'
