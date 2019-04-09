b31227c5eedd2405ed599f41669af871319e540f
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-04-08 20:13:32 -0400

    First commit for this submodule

---
+++ qmspy/graph_data/__init__.py
@@ -0,0 +1,29 @@
+"""
+This is the data graphing submodule of the QMS package, it houses all the
+data plotting functions.
+
+Contains the following functions:
+                line
+                bar
+                scatter
+                dddbar
+                dddline
+
+
+Author: Brian C. Ferrari
+"""
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import LogNorm
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sb
+
+#Data Frame Columns
+amu       =  'mass amu'
+sem       =  'SEM Amps'
+ev        =  'electron-energy V'
+cyc       =  'Cycle'
+col       =  'Color'
+mar       =  'Marker'
+std       =  'Standard Deviation'
