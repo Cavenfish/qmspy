446cab7f8768aff9bbc75e0fdec9cb2beddb630c
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-04-24 11:15:05 -0400

    Moved global variables to config and imported it in using wildcard

--- qmspy/__init__.py
+++ qmspy/__init__.py
@@ -13,27 +13,12 @@ Contains the following submodules:
 
 Author: Brian C. Ferrari
 """
-from itertools import cycle
-import numpy as np
-import pandas as pd
+from .config            import *
 
 from .add_style         import add_style
 from .get_errs          import get_errs
 from .init_data         import init_data
 from .integrate         import integrate
 from .appearance_energy import appearance_energy
+
 from .graph_data        import *
-
-
-#Error Header and Tail
-error_head = "\n*****uh oh spaghettios*****\n"
-error_tail = "\n*****Ponder this, then return to me*****\n"
-
-#Data Frame Columns
-amu       =  'mass amu'
-sem       =  'SEM Amps'
-ev        =  'electron-energy V'
-cyc       =  'Cycle'
-col       =  'Color'
-mar       =  'Marker'
-std       =  'Standard Deviation'
