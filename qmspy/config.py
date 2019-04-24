71916049bfdf5859c18339b408c1bb41a69dfc42
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-04-24 11:14:32 -0400

    First Commit

---
+++ qmspy/config.py
@@ -0,0 +1,16 @@
+from itertools import cycle
+import numpy as np
+import pandas as pd
+
+#Error Header and Tail
+error_head = "\n*****uh oh spaghettios*****\n"
+error_tail = "\n*****Ponder this, then return to me*****\n"
+
+#Data Frame Columns
+amu       =  'mass amu'
+sem       =  'SEM Amps'
+ev        =  'electron-energy V'
+cyc       =  'Cycle'
+col       =  'Color'
+mar       =  'Marker'
+std       =  'Standard Deviation'
