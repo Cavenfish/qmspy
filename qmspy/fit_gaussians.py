71bffd0320f8c5ef44b406fda1f11aaefb169cc1
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-05-02 19:18:28 -0400

    added some comments

--- qmspy/fit_gaussians.py
+++ qmspy/fit_gaussians.py
@@ -27,19 +27,25 @@ def fit_gaussians(data, peaks):
     df[pks]            = 0
     df.loc[peaks][pks] = 1
 
-    #Add gaussian fits column to dataframe
+    #initialize some counter and gao==array for gaussian fit values
     i   = 0
     gao = np.zeros(len(df[sem]))
+
+    #interate through peaks
     for peak in peaks:
-        left   = properties['left_ips'][i]
-        right  = properties['right_ips'][i]
-        length = int(right - left)
-        width  = properties['widths'][i]
+        left   = properties['left_ips'][i]  #left point of gaussian
+        right  = properties['right_ips'][i] #right point of gaussian
+        length = int(right - left)          #length of gaussian
+        width  = properties['widths'][i]    #gaussian width
+
+        #generate gaussian curve fit to peak data
         gaus   = gaussian(length, width) * properties['peak_heights'][i]
 
+        #plug generated gaussian into gao array then increment counter
         gao[left:right] = gaus
         i+=1
 
+    #Add gaussian fits column to dataframe
     df[gau] = gao
 
     return df
