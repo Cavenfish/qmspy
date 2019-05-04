ebf93d244ea806da2ec756068cf7020c4009adf2
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-05-04 12:00:01 -0400

    made it properly formatted

--- qmspy/graph_data/__init__.py
+++ qmspy/graph_data/__init__.py
@@ -15,19 +15,13 @@ Contains the following functions:
 
 Author: Brian C. Ferrari
 """
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.colors import LogNorm
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-import seaborn as sb
+from .config    import *
 
 
-#Data Frame Columns
-amu       =  'mass amu'
-sem       =  'SEM Amps'
-ev        =  'electron-energy V'
-cyc       =  'Cycle'
-col       =  'Color'
-mar       =  'Marker'
-std       =  'Standard Deviation'
+from .bar       import bar
+from .barx3d    import barx3d
+from .contour   import contour
+from .line      import line
+from .line3d    import line3d
+from .scatter   import scatter
+from .scatter3d import scatter3d
