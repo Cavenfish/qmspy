a6a26de368993ee2910dc2f743a507494ed6226c
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-05-04 12:14:30 -0400

    Working properly but can be made better

--- qmspy/graph_data/line.py
+++ qmspy/graph_data/line.py
@@ -1,9 +1,12 @@
+from ..config import *
 
 def line(data, size, ls):
     """
     """
+    df0 = check_data_type(data)
+    
     fig, ax = plt.subplots(figsize=size)
-    for label, df in data.groupby(amu):
+    for label, df in df0.groupby(amu):
         c    = df[col].iloc[1]
         m    = df[mar].iloc[1]
         yerr = df[std].iloc[1]
