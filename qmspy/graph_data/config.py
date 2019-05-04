5023e58e718eb77b5ce0f1ac15e1ed2060d2a5d0
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-05-04 12:00:25 -0400

    First Commit

---
+++ qmspy/graph_data/config.py
@@ -0,0 +1,16 @@
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import LogNorm
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sb
+
+
+#Data Frame Columns
+amu       =  'mass amu'
+sem       =  'SEM Amps'
+ev        =  'electron-energy V'
+cyc       =  'Cycle'
+col       =  'Color'
+mar       =  'Marker'
+std       =  'Standard Deviation'
