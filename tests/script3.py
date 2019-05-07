6221bac6469ec2e1413d31304ba680509ce0b3a6
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-05-07 14:30:07 -0400

    Changed names from scipt to test as they should have been named

--- tests/script3.py
+++
@@ -1,6 +0,0 @@
-import qmspy as qp
-
-data = './Data.csv'
-
-#Add Colors and Markers columns to DataFrame
-df = qp.add_style(data)
--- tests/script8.py
+++
@@ -1,9 +0,0 @@
-import qmspy as qp
-import pandas as pd
-import matplotlib.pyplot as plt
-
-data = pd.read_csv('./Data_with_Style.csv')
-size = (10,8)
-
-fig, ax = qp.scatter3d(data, size)
-plt.show(fig)
