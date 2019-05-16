4482a5f202d44720a3160757891dee2e46d679d9
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-05-16 19:30:02 -0400

    Added the overplot function

--- qmspy/graph_data/__init__.py
+++ qmspy/graph_data/__init__.py
@@ -18,10 +18,11 @@ Author: Brian C. Ferrari
 from ..config   import *
 
 
-from .bar       import bar
-from .barx3d    import barx3d
-from .contour   import contour
-from .line      import line
-from .line3d    import line3d
-from .scatter   import scatter
-from .scatter3d import scatter3d
+from .bar                   import bar
+from .barx3d                import barx3d
+from .contour               import contour
+from .line                  import line
+from .line3d                import line3d
+from .scatter               import scatter
+from .scatter3d             import scatter3d
+from .gaussians_overplotted import gaussians_overplotted
