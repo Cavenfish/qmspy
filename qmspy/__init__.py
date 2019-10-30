473da986ef912509ddf5b671eac6817acef54672
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-10-29 21:44:19 -0400

    added find_peaks function

--- qmspy/__init__.py
+++ qmspy/__init__.py
@@ -19,6 +19,7 @@ from .config            import *
 from .add_style         import add_style
 from .init_data         import init_data
 from .fit_gaussians     import fit_gaussians
+from .find_peaks        import find_peaks
 from .appearance_energy import appearance_energy
 
 from .graph_data        import *
