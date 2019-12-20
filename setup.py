172b720565d3e77815d7b92736ea331b322f5c96
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-12-20 17:17:13 -0500

    Version 0.0.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qmspy",
-    version="0.0.9",
+    version="0.0.10",
     author="Brian C. Ferrari",
     author_email="brianf1996@knights.ucf.edu",
     description="This is a python module for analysing and graphing QMS data.",
